389c0e799191eff6f7f4a4f576404ae34eee5bfc
openshiftio-launchpad <obsidian-leadership@redhat.com>
2017-12-04 18:26:36 +0000

    Initial commit

---
+++ test/greeting-test.js
@@ -0,0 +1,9 @@
+const test = require('tape');
+// const supertest = require('supertest');
+
+// const app = require('../app');
+
+test('test needs implementing', (t) => {
+  t.pass('Needs a Test');
+  t.end();
+});
